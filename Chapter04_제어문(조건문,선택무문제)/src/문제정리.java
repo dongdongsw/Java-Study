5a304d0aa6107065174ed93fb7a1e58cd54aba8b
dongdongsw <fishman4535@gmail.com>
2025-07-28 17:51:21 +0900

    5일차__2

---
+++ Chapter04_제어문(조건문,선택무문제)/src/문제정리.java
@@ -0,0 +1,19 @@
+
+
+/*
+ * 	1. 형식 
+ * 		ㄴ 단일 조건문 if() -> 조건이  true일때만 실행 false이면 건너뛴다
+ * 		ㄴ equals = 문자 비교 / contains = 포함되어있는지 / startsWith = 시작문자 / endsWith = 끝문자 / 
+ *  
+ *  	ㄴif ~ else -> 114page
+ *  	ㄴ다중 조건문 => 115page
+ * 	
+ */
+public class 문제정리 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
